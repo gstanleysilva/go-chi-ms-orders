966bebe0085119115d290a07e99e5e995ef3126b
gustavo <guhstanley@gmail.com>
2023-10-10 19:45:02 -0300

    basic http server

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("failed to listen to server", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		if r.URL.Path == "/sayHello" {
+			w.Write([]byte("Hello world!"))
+		}
+	}
 }
