70f8672938b0291bd2f2846d84dc3e6c07bb9d7e
gustavo <guhstanley@gmail.com>
2023-10-11 20:18:31 -0300

    adding safe shutdown and context

--- application/app.go
+++ application/app.go
@@ -4,30 +4,68 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 
 	return app
 }
 
 func (a *App) Start(ctx context.Context) error {
+	//Create our Http Server
 	server := &http.Server{
 		Addr:    ":3000",
 		Handler: a.router,
 	}
 
-	err := server.ListenAndServe()
+	//Check Redis connection
+	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
+		return fmt.Errorf("failed to connect to redis: %w", err)
 	}
 
-	return nil
+	//Closes Redis
+	defer func() {
+		if err := a.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis", err)
+		}
+	}()
+
+	fmt.Println("Starting server on port:", "3000")
+
+	ch := make(chan error, 1)
+
+	//Execute the server on a separate thread so we can perform the following select switch
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}()
+
+	//Listens to an error from the server or context termination from Interrupt event
+	//If any of these scenarios happen we call the shutdown method of our server
+	//with a timeout context so it can have time to any in flight requests to resolve
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		return server.Shutdown(timeout)
+	}
 }
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/guhstanley/go-chi-ms-orders/application"
 )
@@ -10,7 +12,13 @@ import (
 func main() {
 	app := application.New()
 
-	err := app.Start(context.TODO())
+	//Creates a base context for the app based on a root level and a signal that will be
+	//notified if an interruption occur
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	//Cancell all chield level contexts at the end of the function
+	defer cancel()
+
+	err := app.Start(ctx)
 
 	if err != nil {
 		fmt.Println("failed to start app:", err)
